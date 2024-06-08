39368ae13ffbc02ba413786699fb517792ad2f34
Danilo <danilogmoura@gmail.com>
2024-06-08 18:02:10 -0300

    Update V013__cria-coluna-data-atualizacao-forma-pagto.sql

--- algafood-api/src/main/resources/db/migration/V013__cria-coluna-data-atualizacao-forma-pagto.sql
+++ algafood-api/src/main/resources/db/migration/V013__cria-coluna-data-atualizacao-forma-pagto.sql
@@ -6,3 +6,13 @@ update forma_pagamento
 
 alter table forma_pagamento
     modify data_atualizacao datetime not null;
+
+alter table forma_pagamento
+    add data_cadastro datetime null;
+
+update forma_pagamento
+    set data_cadastro = utc_timestamp;
+
+alter table forma_pagamento
+    modify data_cadastro datetime not null;
+
