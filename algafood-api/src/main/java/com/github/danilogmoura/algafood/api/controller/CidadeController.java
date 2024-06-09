113dc35180da5ed6e5a181c6843166112d9e2d34
Danilo <danilogmoura@gmail.com>
2024-06-09 04:41:34 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/CidadeController.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package com.github.danilogmoura.algafood.api.controller;
 
 import com.github.danilogmoura.algafood.api.assembler.CidadeInputDisassembler;
 import com.github.danilogmoura.algafood.api.assembler.CidadeModelAssembler;
+import com.github.danilogmoura.algafood.api.exceptionhandler.Problem;
 import com.github.danilogmoura.algafood.api.model.CidadeModel;
 import com.github.danilogmoura.algafood.api.model.input.CidadeInput;
 import com.github.danilogmoura.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -11,6 +12,8 @@ import com.github.danilogmoura.algafood.domain.service.CidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +52,10 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade por ID")
+    @ApiResponses({
+        @ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     @GetMapping("/{cidadeId}")
     public CidadeModel buscar(
         @ApiParam(value = "ID de uma cidade", example = "1")
@@ -61,6 +68,9 @@ public class CidadeController {
     @ApiOperation("Cadastra uma cidade")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({
+        @ApiResponse(code = 201, message = "Cidade cadastrada")
+    })
     public CidadeModel adicionar(
         @ApiParam(name = "corpo", value = "Representação de uma nova cidade")
         @RequestBody @Valid CidadeInput cidadeInput) {
@@ -75,6 +85,10 @@ public class CidadeController {
 
     @ApiOperation("Cadastra uma cidade por ID")
     @PutMapping("/{cidadeId}")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Cidade Atualizada"),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     public CidadeModel atualizar(
         @ApiParam(value = "ID de uma cidade", example = "1")
         @PathVariable Long cidadeId,
@@ -95,6 +109,10 @@ public class CidadeController {
     @ApiOperation("Exclui uma cidade por ID")
     @DeleteMapping("/{cidadeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Cidade excluída"),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
+    })
     public void remover(
         @ApiParam(value = "ID de uma cidade", example = "1")
         @PathVariable Long cidadeId) {
--- algafood-api/src/main/java/com/github/danilogmoura/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/core/openapi/SpringFoxConfig.java
@@ -5,14 +5,17 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.danilogmoura.algafood.api.exceptionhandler.Problem;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -65,10 +68,14 @@ public class SpringFoxConfig {
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                 .description("Requisição inválida (erro do cliente)")
+                .representation(MediaType.APPLICATION_JSON)
+                .apply(getProblemaModelReference())
                 .build(),
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                 .description("Erro interno no servidor")
+                .representation(MediaType.APPLICATION_JSON)
+                .apply(getProblemaModelReference())
                 .build(),
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
@@ -77,6 +84,8 @@ public class SpringFoxConfig {
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
                 .description("Requisição recusada porque o corpo está em um formato não suportado")
+                .representation(MediaType.APPLICATION_JSON)
+                .apply(getProblemaModelReference())
                 .build()
         );
     }
@@ -86,10 +95,14 @@ public class SpringFoxConfig {
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                 .description("Requisição inválida (erro do cliente)")
+                .representation(MediaType.APPLICATION_JSON)
+                .apply(getProblemaModelReference())
                 .build(),
             new ResponseBuilder()
                 .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
                 .description("Erro interno no servidor")
+                .representation(MediaType.APPLICATION_JSON)
+                .apply(getProblemaModelReference())
                 .build()
         );
     }
@@ -106,6 +119,12 @@ public class SpringFoxConfig {
     @Bean
     public JacksonModuleRegistrar springFoxJacksonConfig() {
         return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    private Consumer<RepresentationBuilder> getProblemaModelReference() {
+        return r -> r.model(m -> m.name("Problema")
+            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+                q -> q.name("Problema").namespace("com.github.danilogmoura.algafood.api.exceptionhandler")))));
     }
 
 }
