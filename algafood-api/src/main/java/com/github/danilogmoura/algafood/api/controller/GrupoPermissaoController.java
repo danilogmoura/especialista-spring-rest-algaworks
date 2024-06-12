524998e8bb92e2a1edfff4605814062b76cf2121
Danilo <danilogmoura@gmail.com>
2024-06-12 19:04:36 -0300

    Desafio: descrevendo documentação de endpoints de associação de permissões com grupos

--- algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/GrupoPermissaoController.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/GrupoPermissaoController.java
@@ -2,10 +2,12 @@ package com.github.danilogmoura.algafood.api.controller;
 
 import com.github.danilogmoura.algafood.api.assembler.PermissaoModelAssembler;
 import com.github.danilogmoura.algafood.api.model.PermissaoModel;
+import com.github.danilogmoura.algafood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.github.danilogmoura.algafood.domain.service.GrupoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
 
     @Autowired
@@ -26,7 +28,7 @@ public class GrupoPermissaoController {
     private PermissaoModelAssembler permissaoModelAssembler;
 
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PermissaoModel> listar(@PathVariable Long grupoId) {
         var grupo = grupoService.buscarOuFalhar(grupoId);
         return permissaoModelAssembler.toCollectionModel(grupo.getPermissoes());
