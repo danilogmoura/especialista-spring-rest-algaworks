ada1e57bea428a925f0706aef33db749437ef8a6
Danilo <danilogmoura@gmail.com>
2024-06-11 22:37:43 -0300

    Desafio: descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -2,10 +2,12 @@ package com.github.danilogmoura.algafood.api.controller;
 
 import com.github.danilogmoura.algafood.api.assembler.FormaPagamentoModelAssembler;
 import com.github.danilogmoura.algafood.api.model.FormaPagamentoModel;
+import com.github.danilogmoura.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.github.danilogmoura.algafood.domain.service.RestauranteService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     @Autowired
     private RestauranteService restauranteService;
@@ -25,14 +27,14 @@ public class RestauranteFormaPagamentoController {
     @Autowired
     private FormaPagamentoModelAssembler formaPagamentoModelAssembler;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<FormaPagamentoModel> listar(@PathVariable Long restauranteId) {
         var restaurante = restauranteService.buscarOuFalhar(restauranteId);
         var formasPagamento = restaurante.getFormasPagamento();
         return formaPagamentoModelAssembler.toCollectionModel(formasPagamento);
     }
 
-    @DeleteMapping("/{formaPagamentoId}")
+    @DeleteMapping(path = "/{formaPagamentoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
         restauranteService.desassociarFormaPagamento(restauranteId, formaPagamentoId);
