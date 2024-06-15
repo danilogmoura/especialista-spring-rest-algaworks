0b7b48e65ddc19f91e3785985383844a47fbc3a8
Danilo <danilogmoura@gmail.com>
2024-06-15 00:03:57 -0300

    Adicionando hypermedia em recursos com paginação

--- algafood-api/src/main/java/com/github/danilogmoura/algafood/api/assembler/CozinhaModelAssembler.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/api/assembler/CozinhaModelAssembler.java
@@ -1,23 +1,33 @@
 package com.github.danilogmoura.algafood.api.assembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import com.github.danilogmoura.algafood.api.controller.CozinhaController;
 import com.github.danilogmoura.algafood.api.model.CozinhaModel;
 import com.github.danilogmoura.algafood.domain.model.Cozinha;
-import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CozinhaModelAssembler {
+public class CozinhaModelAssembler extends RepresentationModelAssemblerSupport<Cozinha, CozinhaModel> {
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public CozinhaModel toModel(Cozinha cozinha) {
-        return modelMapper.map(cozinha, CozinhaModel.class);
+    public CozinhaModelAssembler() {
+        super(CozinhaController.class, CozinhaModel.class);
     }
 
-    public List<CozinhaModel> toCollectionModel(List<Cozinha> cozinhas) {
-        return cozinhas.stream().map(this::toModel).toList();
+    @Override
+    public CozinhaModel toModel(Cozinha cozinha) {
+        var cozinhaModel = createModelWithId(cozinha.getId(), cozinha);
+
+        modelMapper.map(cozinha, cozinhaModel);
+
+        cozinhaModel.add(linkTo(CozinhaController.class).withRel("cozinhas"));
+
+        return cozinhaModel;
     }
 }
--- algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/api/controller/CozinhaController.java
@@ -5,14 +5,15 @@ import com.github.danilogmoura.algafood.api.assembler.CozinhaModelAssembler;
 import com.github.danilogmoura.algafood.api.model.CozinhaModel;
 import com.github.danilogmoura.algafood.api.model.input.CozinhaInput;
 import com.github.danilogmoura.algafood.api.openapi.controller.CozinhaControllerOpenApi;
+import com.github.danilogmoura.algafood.domain.model.Cozinha;
 import com.github.danilogmoura.algafood.domain.repository.CozinhaRepository;
 import com.github.danilogmoura.algafood.domain.service.CozinhaService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,11 +42,15 @@ public class CozinhaController implements CozinhaControllerOpenApi {
     @Autowired
     private CozinhaInputDisassembler cozinhaInputDisassembler;
 
+    @Autowired
+    private PagedResourcesAssembler<Cozinha> pagedResourcesAssembler;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
+    public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
         var cozinhasPage = cozinhaRepository.findAll(pageable);
-        var cozinhasModel = cozinhaModelAssembler.toCollectionModel(cozinhasPage.getContent());
-        return new PageImpl<>(cozinhasModel, pageable, cozinhasPage.getTotalElements());
+
+        return pagedResourcesAssembler
+            .toModel(cozinhasPage, cozinhaModelAssembler);
     }
 
     @GetMapping(path = "/{cozinhaId}", produces = MediaType.APPLICATION_JSON_VALUE)
