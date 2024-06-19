2b74fa903ea999f6f6aaafbc7108bae544ebbc3f
Danilo <danilogmoura@gmail.com>
2024-06-19 00:44:56 -0300

    Update ApiRetirementHandler.java

--- algafood-api/src/main/java/com/github/danilogmoura/algafood/core/web/ApiRetirementHandler.java
+++ algafood-api/src/main/java/com/github/danilogmoura/algafood/core/web/ApiRetirementHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-@Component
+//@Component
 public class ApiRetirementHandler implements HandlerInterceptor {
 
     @Override
