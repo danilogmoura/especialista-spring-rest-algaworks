11c92923ef96a73158f6dd840fc3dadc9f9d236c
Danilo <danilogmoura@gmail.com>
2024-06-30 05:02:04 -0300

    Configurando o Resource Server para JWT assinado com chave simétrica

--- algafood-auth/src/main/java/com/github/danilogmoura/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/github/danilogmoura/algafood/auth/AuthorizationServerConfig.java
@@ -109,7 +109,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
 
-        jwtAccessTokenConverter.setSigningKey("algaworks");
+        jwtAccessTokenConverter.setSigningKey("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s");
 
         return jwtAccessTokenConverter;
     }
